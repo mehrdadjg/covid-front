ac25210091a8e37af8c4cfdead96f4341f100037
Mehrdad Jafarigiv <mehrdadjg@gmail.com>
2020-10-31 17:58:24 -0600

    separated IconedInputField into a new module

---
+++ src/components/IconedInputField.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import Lottie from "lottie-web";
+
+import { IconButton, MenuItem, TextField, Tooltip } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+export default function IconedInputField(props) {
+  const {
+    type,
+    label,
+    items,
+    inputValue,
+    inputError,
+    icon,
+    iconId,
+    iconIsButton,
+    onIconClick,
+    iconMessage,
+    ...other
+  } = props;
+  const classes = useClasses();
+
+  let input = null;
+  if (type === "text") {
+    input = (
+      <TextField
+        label={label}
+        variant="outlined"
+        InputLabelProps={{
+          classes: {
+            asterisk: classes.asterisk,
+          },
+        }}
+        fullWidth
+        value={inputValue}
+        error={inputError}
+        {...other}
+      />
+    );
+  } else if (type === "select") {
+    input = (
+      <TextField
+        label={label}
+        variant="outlined"
+        select
+        SelectProps={{
+          MenuProps: {
+            disableScrollLock: true,
+          },
+        }}
+        InputLabelProps={{
+          classes: {
+            asterisk: classes.asterisk,
+          },
+        }}
+        fullWidth
+        value={inputValue}
+        error={inputError}
+        {...other}
+      >
+        {items.map((item, index) => (
+          <MenuItem key={index} value={index}>
+            {item}
+          </MenuItem>
+        ))}
+      </TextField>
+    );
+  }
+
+  const [iconProperties, setIconProperties] = useState({
+    size: 0,
+    opacity: 0,
+  });
+
+  const iconContainer = useRef(null);
+  useEffect(() => {
+    if (icon === "error" || icon === "warning") {
+      setIconProperties((old) => ({
+        ...old,
+        opacity: 1,
+        size: 40,
+      }));
+      const animation =
+        icon === "error"
+          ? require("../animations/error.json")
+          : require("../animations/warning.json");
+      Lottie.loadAnimation({
+        name: iconId,
+        container: iconContainer.current,
+        renderer: "svg",
+        loop: false,
+        autoplay: true,
+        animationData: animation,
+      });
+    } else if (icon === "done") {
+      setIconProperties((old) => ({
+        ...old,
+        opacity: 1,
+        size: 40,
+      }));
+      const errorAnimation = require("../animations/done.json");
+      Lottie.loadAnimation({
+        name: iconId,
+        container: iconContainer.current,
+        renderer: "svg",
+        loop: false,
+        autoplay: true,
+        animationData: errorAnimation,
+      }).addEventListener("complete", () => {
+        setIconProperties((old) => ({
+          ...old,
+          opacity: 0,
+          size: 0,
+        }));
+        setTimeout(() => {
+          Lottie.destroy(iconId);
+        }, 250);
+      });
+    } else {
+      setIconProperties((old) => ({
+        ...old,
+        opacity: 0,
+        size: 0,
+      }));
+      setTimeout(() => {
+        Lottie.destroy(iconId);
+      }, 250);
+    }
+  }, [icon, iconId]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      {input}
+      <Tooltip title={iconMessage} placement="left">
+        {iconIsButton ? (
+          <IconButton
+            color="primary"
+            classes={{ root: classes.iconButton }}
+            onClick={onIconClick}
+          >
+            <div
+              style={{
+                width: `${iconProperties.size}px`,
+                height: `${iconProperties.size}px`,
+                opacity: iconProperties.opacity,
+                overflow: "hidden",
+                transition: "all 0.25s ease-out",
+              }}
+              ref={iconContainer}
+            />
+          </IconButton>
+        ) : (
+          <div
+            style={{
+              width: `${iconProperties.size}px`,
+              height: `${iconProperties.size}px`,
+              opacity: iconProperties.opacity,
+              overflow: "hidden",
+              transition: "all 0.25s ease-out",
+            }}
+            ref={iconContainer}
+          />
+        )}
+      </Tooltip>
+    </div>
+  );
+}
+
+const useClasses = makeStyles((theme) => ({
+  asterisk: {
+    color: "red",
+  },
+  iconButton: { padding: "0" },
+}));
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import Lottie from "lottie-web";
-
 import { setProfile } from "../actions/business";
+import IconedInputField from "./IconedInputField";
 
-import {
-  Grid,
-  IconButton,
-  MenuItem,
-  TextField,
-  Tooltip,
-  Typography,
-} from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { green } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -749,174 +741,6 @@ export default function Profile() {
   );
 }
 
-const IconedInputField = (props) => {
-  const {
-    type,
-    label,
-    items,
-    inputValue,
-    inputError,
-    icon,
-    iconId,
-    iconIsButton,
-    onIconClick,
-    iconMessage,
-    ...other
-  } = props;
-  const classes = useClasses();
-
-  let input = null;
-  if (type === "text") {
-    input = (
-      <TextField
-        label={label}
-        variant="outlined"
-        InputLabelProps={{
-          classes: {
-            asterisk: classes.asterisk,
-          },
-        }}
-        fullWidth
-        value={inputValue}
-        error={inputError}
-        {...other}
-      />
-    );
-  } else if (type === "select") {
-    input = (
-      <TextField
-        label={label}
-        variant="outlined"
-        select
-        SelectProps={{
-          MenuProps: {
-            disableScrollLock: true,
-          },
-        }}
-        InputLabelProps={{
-          classes: {
-            asterisk: classes.asterisk,
-          },
-        }}
-        fullWidth
-        value={inputValue}
-        error={inputError}
-        {...other}
-      >
-        {items.map((item, index) => (
-          <MenuItem key={index} value={index}>
-            {item}
-          </MenuItem>
-        ))}
-      </TextField>
-    );
-  }
-
-  const [iconProperties, setIconProperties] = useState({
-    size: 0,
-    opacity: 0,
-  });
-
-  const iconContainer = useRef(null);
-  useEffect(() => {
-    if (icon === "error" || icon === "warning") {
-      setIconProperties((old) => ({
-        ...old,
-        opacity: 1,
-        size: 40,
-      }));
-      const animation =
-        icon === "error"
-          ? require("../animations/error.json")
-          : require("../animations/warning.json");
-      Lottie.loadAnimation({
-        name: iconId,
-        container: iconContainer.current,
-        renderer: "svg",
-        loop: false,
-        autoplay: true,
-        animationData: animation,
-      });
-    } else if (icon === "done") {
-      setIconProperties((old) => ({
-        ...old,
-        opacity: 1,
-        size: 40,
-      }));
-      const errorAnimation = require("../animations/done.json");
-      Lottie.loadAnimation({
-        name: iconId,
-        container: iconContainer.current,
-        renderer: "svg",
-        loop: false,
-        autoplay: true,
-        animationData: errorAnimation,
-      }).addEventListener("complete", () => {
-        setIconProperties((old) => ({
-          ...old,
-          opacity: 0,
-          size: 0,
-        }));
-        setTimeout(() => {
-          Lottie.destroy(iconId);
-        }, 250);
-      });
-    } else {
-      setIconProperties((old) => ({
-        ...old,
-        opacity: 0,
-        size: 0,
-      }));
-      setTimeout(() => {
-        Lottie.destroy(iconId);
-      }, 250);
-    }
-  }, [icon, iconId]);
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-      }}
-    >
-      {input}
-      <Tooltip title={iconMessage} placement="left">
-        {iconIsButton ? (
-          <IconButton
-            color="primary"
-            classes={{ root: classes.iconButton }}
-            onClick={onIconClick}
-          >
-            <div
-              style={{
-                width: `${iconProperties.size}px`,
-                height: `${iconProperties.size}px`,
-                opacity: iconProperties.opacity,
-                overflow: "hidden",
-                transition: "all 0.25s ease-out",
-              }}
-              ref={iconContainer}
-            />
-          </IconButton>
-        ) : (
-          <div
-            style={{
-              width: `${iconProperties.size}px`,
-              height: `${iconProperties.size}px`,
-              opacity: iconProperties.opacity,
-              overflow: "hidden",
-              transition: "all 0.25s ease-out",
-            }}
-            ref={iconContainer}
-          />
-        )}
-      </Tooltip>
-    </div>
-  );
-};
-
 const useClasses = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(1),
@@ -936,8 +760,4 @@ const useClasses = makeStyles((theme) => ({
     marginTop: -12,
     marginLeft: -12,
   },
-  asterisk: {
-    color: "red",
-  },
-  iconButton: { padding: "0" },
 }));
