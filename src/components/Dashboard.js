576dd743f3a446fb0a9289a3e362cab339543557
Mehrdad Jafarigiv <mehrdadjg@gmail.com>
2020-11-13 14:53:00 -0700

    added logout feature

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,81 @@
 import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
+import { useDispatch } from "react-redux";
+
+import { removeCookie } from "../cookies";
+import { signOut } from "../actions/signedIn";
+import { resetBusiness } from "../actions/business";
+import { resetAuthentication } from "../actions/authentication";
+import { showAlert as showAlertAction, hideAlert } from "../actions/alert";
 
 import BusinessOverview from "./BusinessOverview";
-import Profile from "./Profile";
 import Customize from "./Customize";
+import Profile from "./Profile";
+import Visits from "./Visits";
 
-import { FormControl, MenuItem, Select, Tab, Tabs } from "@material-ui/core";
+import {
+  Button,
+  FormControl,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  MenuItem,
+  Select,
+  Tab,
+  Tabs,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 export default function Dashboard() {
+  const dispatcher = useDispatch();
+
+  function showAlert(message, mode, duration = 3000) {
+    dispatcher(showAlertAction(message, mode));
+    setTimeout(() => {
+      dispatcher(hideAlert());
+    }, duration);
+  }
+
+  const [logoutDialogOpen, setLogoutDialogOpen] = useState(false);
+  const handleDialogClose = () => {
+    setLogoutDialogOpen(false);
+  };
+  const handleDialogYes = () => {
+    removeCookie("business_logged_in");
+    removeCookie("auth");
+
+    dispatcher(resetBusiness());
+    dispatcher(resetAuthentication());
+    dispatcher(signOut());
+
+    showAlert("Logout Successful.", "error");
+  };
+
   const [tabIndex, setTabIndex] = useState(0);
   const handleTabChange = (event, newValue) => {
-    setTabIndex(newValue);
+    if (newValue < menuItems.length) {
+      setTabIndex(newValue);
+    } else if (newValue === menuItems.length) {
+      setLogoutDialogOpen(() => true);
+    }
   };
   const handleSelectChange = (event) => {
-    setTabIndex(event.target.value);
+    console.log(event.target.value);
+    if (event.target.value < menuItems.length) {
+      setTabIndex(event.target.value);
+    } else if (event.target.value === menuItems.length) {
+      setLogoutDialogOpen(() => true);
+    }
   };
 
   const menuItems = [
     { name: "Overview", index: 0 },
     { name: "Profile", index: 1 },
     { name: "Customization", index: 2 },
-    { name: "Item Four", index: 3 },
+    { name: "View Visits", index: 3 },
     { name: "Item Five", index: 4 },
     { name: "Item Six", index: 5 },
-    { name: "Item Seven", index: 6 },
   ];
 
   const isSmallScreen = useMediaQuery({ maxWidth: 850 });
@@ -39,10 +90,20 @@ export default function Dashboard() {
         <FormControl variant="outlined" className={classes.formControl}>
           <Select value={tabIndex} onChange={handleSelectChange}>
             {menuItems.map((item) => (
-              <MenuItem key={item.index} value={item.index}>
+              <MenuItem
+                classes={{ root: classes.menuItems }}
+                key={item.index}
+                value={item.index}
+              >
                 {item.name}
               </MenuItem>
             ))}
+            <MenuItem
+              classes={{ root: classes.logout }}
+              value={menuItems.length}
+            >
+              Logout
+            </MenuItem>
           </Select>
         </FormControl>
       ) : (
@@ -56,6 +117,7 @@ export default function Dashboard() {
           {menuItems.map((item) => (
             <Tab key={item.index} label={item.name} />
           ))}
+          <Tab classes={{ textColorInherit: classes.logout }} label="Logout" />
         </Tabs>
       )}
       <TabPanel
@@ -82,6 +144,35 @@ export default function Dashboard() {
       >
         <Customize />
       </TabPanel>
+      <TabPanel
+        currentTab={tabIndex}
+        index={3}
+        persist={true}
+        isSmallScreen={isSmallScreen}
+      >
+        <Visits />
+      </TabPanel>
+
+      <Dialog open={logoutDialogOpen} onClose={handleDialogClose}>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to logout?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={handleDialogClose}
+            autoFocus
+          >
+            No
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleDialogYes}>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
@@ -132,5 +223,12 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
     maxWidth: "1300px",
     width: "100%",
+  },
+  logout: {
+    color: theme.palette.error.main,
+    justifyContent: "center",
+  },
+  menuItems: {
+    justifyContent: "center",
   },
 }));
